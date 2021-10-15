3523ea07cfe514036f7776c9849cd437e643307a
VanLance <codingtempletest@gmail.com>
2021-10-14 19:48:41 -0500

    pushing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //Components
 import Header from './components/Header';
+import Home from './components/Home'
 
 // Styles
 import { GlobalStyle } from './GlobalStyle';
@@ -11,7 +12,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      Start here.
+      <Home />
+      
       <GlobalStyle />
     </div>
   );
